9573ec8ad328d1408dc62354185d86ad4b02d7c4
pparaska <45350812+pparaska@users.noreply.github.com>
2018-12-12 20:03:06 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+class Hello1{
+
+	static void display()
+	{
+		System.out.println("disp");
+	}
+	public static void main(String[] args)
+	{
+		display();
+		System.out.println("Hello World");
+		display();
+	}
+}
